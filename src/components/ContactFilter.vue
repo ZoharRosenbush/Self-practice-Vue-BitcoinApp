2bdd0dccdee1a5f112d33040c1cd3977b38afa8c
ZoharRo <zozozohar@gmail.com>
2022-02-13 21:46:22 +0200

    added filter

---
+++ src/components/ContactFilter.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            filterBy: {
+                name: '',
+                phone: ''
+            }
+        }
+    },
+    methods: {
+        setContactFilter() {
+            this.$emit('set-filter', this.filterBy)
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="filter">
+        <input type="text" placeholder="Name" v-model="filterBy.name" @input="setContactFilter" />
+        <input
+            type="number"
+            placeholder="Telephone"
+            v-model="filterBy.phone"
+            @input="setContactFilter"
+        />
+    </section>
+</template>
--- src/views/ContactApp.vue
+++ src/views/ContactApp.vue
@@ -1,24 +1,48 @@
 <script>
 import { contactService } from "../services/contact.service"
 import ContactList from "../components/ContactList.vue"
+import ContactFilter from "../components/ContactFilter.vue"
+
 export default {
     components: {
         ContactList,
+        ContactFilter
 
     },
     data() {
         return {
-            contacts: []
+            contacts: [],
+            currFilterBy: null
         }
     },
     async created() {
         this.contacts = await contactService.query()
     },
+    methods: {
+        setFilter(filterBy) {
+            console.log('the filter by', filterBy)
+            console.log('the contactrs', this.contacts);
+            this.currFilterBy = filterBy
+
+        }
+
+    },
+    computed: {
+        contactsToShow() {
+            if (!this.currFilterBy) return this.contacts
+            const regex = new RegExp(this.currFilterBy.name, 'i')
+            return this.contacts.filter(contact => {
+                return regex.test(contact.name) && contact.phone.includes('' + this.currFilterBy.phone)
+            })
+
+        }
+    }
 }
 </script>
 
 <template>
     <section class="contact-app">
-        <ContactList :contacts="contacts" />
+        <ContactFilter @set-filter="setFilter" />
+        <ContactList :contacts="contactsToShow" />
     </section>
 </template>
