d75629f98f620a026c501e213747956ef482440f
ZoharRo <zozozohar@gmail.com>
2022-02-13 15:29:20 +0200

    app init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+import NewCmp from './components/NewCmp.vue';
+
+export default {
+  data() {
+    return {}
+
+  },
+  components: {
+    NewCmp,
+  },
+}
+</script>
+
+<template>
+  <h1>Zohar</h1>
+  <NewCmp />
+</template>
---
+++ src/components/NewCmp.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<template>
+<h1>hello!</h1>
+</template>
